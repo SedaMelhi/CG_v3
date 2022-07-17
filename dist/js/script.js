adc7bb31cbdc69a6d9d8dd8613f5f581b0591892
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-17 23:36:14 +0300

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,8 @@
-let arch1 = document.querySelector(".arch__left"),
-    arch2 = document.querySelector(".arch__right"),
+let archs = document.querySelectorAll(".arch"),
     content = document.querySelector(".border_second"),
     darkWrap = document.querySelector(".dark-wrap"),
     opacity = 1;
 content.addEventListener("scroll", function() {
-  arch1.scrollTop = content.scrollTop;
-  arch2.scrollTop = content.scrollTop;
+  archs.forEach(arch => arch.scrollTop = content.scrollTop);
 });
 
