2999507c8a800d1ecc434617ca306842a785c9fa
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-19 11:10:59 +0300

    .

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,7 +3,7 @@ let archs = document.querySelectorAll(".arch"),
     darkWrap = document.querySelector(".dark-wrap"),
     main2 = document.querySelector(".main2");
 main2.scrollHeight = archs[1].scrollHeight;
-content.addEventListener("wheel", function() {
+
+content.addEventListener("scroll", function() {
   archs.forEach(arch => arch.scrollTop = content.scrollTop);
 });
-
