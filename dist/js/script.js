fb1a52c6bfc01a927446c6c126f79837b1c1893c
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-19 17:55:56 +0300

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,21 @@
 let archs = document.querySelectorAll(".arch"),
-    content = document.querySelector(".border_second"),
-    darkWrap = document.querySelector(".dark-wrap"),
-    main2 = document.querySelector(".main2");
-main2.scrollHeight = archs[1].scrollHeight;
+    content = document.querySelector(".border_second");
 
 content.addEventListener("scroll", function() {
   archs.forEach(arch => arch.scrollTop = content.scrollTop);
+});
+
+let artists = document.querySelector(".artists"),
+    cards = document.querySelector(".cards"),
+    expect = document.querySelector(".expect__text");
+
+cards.addEventListener("click", function() {
+  archs[1].classList.toggle("arch__middle_anim")
+  cards.classList.toggle("cards__hidden");
+  expect.classList.toggle("expect__hidden")
+  artists.classList.toggle("artists__visible")
+  
+  setTimeout(() => expect.style.display = "none", 550)
+  setTimeout(() => cards.style.display = "none", 550)
+  
 });
