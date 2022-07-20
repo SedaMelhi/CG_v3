2f866324a436dd0a43e59a3de90e3c7841e72500
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-20 19:33:36 +0300

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,15 +7,27 @@ content.addEventListener("scroll", function() {
 
 let artists = document.querySelector(".artists"),
     cards = document.querySelector(".cards"),
-    expect = document.querySelector(".expect__text");
+    expect = document.querySelector(".expect__text"),
+    close = document.querySelector(".artists__close");
 
 cards.addEventListener("click", function() {
-  archs[1].classList.toggle("arch__middle_anim")
+  archs[1].classList.toggle("arch__middle_anim");
   cards.classList.toggle("cards__hidden");
+  expect.classList.toggle("expect__hidden");
+  artists.classList.toggle("artists__visible");
+  
+  setTimeout(() => expect.classList.toggle("expect__hidden_none"), 550)
+  setTimeout(() => cards.style.display = "none", 550)
+  
+});
+
+close.addEventListener("click", function() {
+  archs[1].classList.toggle("arch__middle_anim")
+  cards.classList.toggle("cards__hidden")
   expect.classList.toggle("expect__hidden")
   artists.classList.toggle("artists__visible")
   
-  setTimeout(() => expect.style.display = "none", 550)
-  setTimeout(() => cards.style.display = "none", 550)
+  setTimeout(() => expect.classList.toggle("expect__hidden_none"), 550)
+  setTimeout(() => cards.style.display = "flex", 550)
   
 });
