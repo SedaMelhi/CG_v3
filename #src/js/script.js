211c68a2911f4d62dcb566f8dfa72e121ff6e608
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-20 21:55:36 +0300

    .

--- #src/js/script.js
+++ #src/js/script.js
@@ -10,24 +10,38 @@ let artists = document.querySelector(".artists"),
     expect = document.querySelector(".expect__text"),
     close = document.querySelector(".artists__close");
 
+artists.style.display = "none";
+
 cards.addEventListener("click", function() {
   archs[1].classList.toggle("arch__middle_anim");
-  cards.classList.toggle("cards__hidden");
-  expect.classList.toggle("expect__hidden");
+  cards.classList.toggle("cards__hidden")
+  cards.classList.toggle("cards__visible")
+
+  expect.classList.toggle("expect__hidden")
+  expect.classList.toggle("expect__visible")
+
+  artists.style.display = "block";
   artists.classList.toggle("artists__visible");
-  
-  setTimeout(() => expect.classList.toggle("expect__hidden_none"), 550)
-  setTimeout(() => cards.style.display = "none", 550)
+  artists.classList.toggle("artists__hidden")
+
+  setTimeout(() => expect.style.display = "none", 900)
+  setTimeout(() => cards.style.display = "none", 900)
   
 });
 
 close.addEventListener("click", function() {
   archs[1].classList.toggle("arch__middle_anim")
+
   cards.classList.toggle("cards__hidden")
-  expect.classList.toggle("expect__hidden")
+  cards.classList.toggle("cards__visible")
+  cards.style.display = "flex";
+
   artists.classList.toggle("artists__visible")
-  
-  setTimeout(() => expect.classList.toggle("expect__hidden_none"), 550)
-  setTimeout(() => cards.style.display = "flex", 550)
-  
+  artists.classList.toggle("artists__hidden")
+
+  expect.classList.toggle("expect__hidden")
+  expect.classList.toggle("expect__visible")
+  expect.style.display = "block";
+
+  setTimeout(() => artists.style.display = "none", 800)
 });
