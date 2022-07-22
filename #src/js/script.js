d94d06ee80d87d9efd513320de2a177a8be86e73
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-07-22 22:03:32 +0300

    .

--- #src/js/script.js
+++ #src/js/script.js
@@ -83,15 +83,4 @@ close.addEventListener("click", function() {
 
 
 
-let balls = document.querySelectorAll(".map__circle");
-
-map.addEventListener("mousemove", function(e) {
-   balls.forEach(ball => {
-      let x = ball.getBoundingClientRect().x
-      let y = ball.getBoundingClientRect().y
-      ball.style.left = e.pageX  - ball.offsetWidth / 2 -  x + 'px';
-      ball.style.top = e.pageY - ball.offsetHeight /2 - y + 'px';
-   })
-})
-
 
